ee3f408086e0d43f79b576f979cc6f590adb5bb5
Tayo <cetydistjoy@gmail.com>
2025-04-03 04:17:35 +0100

    initial commit with empty modules

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Compose Navigation"
+include(":app")
+include(":bottomnavigation")
+include(":core:features")
+include(":feature:splash")
+include(":feature:help")
+include(":feature:home")
+include(":feature:profle")
